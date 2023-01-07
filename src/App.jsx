32fafb4cc5dcddd8d9adb5d7bf7ec841f75f77de
Amas9nine <amasdina2@gmail.com>
2023-01-07 13:40:09 +0600

    feat:everithing is ready for checking,separate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useLayoutEffect, useState } from 'react'
+import { useLayoutEffect, useState, useMemo } from 'react'
 import './App.css'
 import { Sum } from './components/Sum';
 
@@ -7,32 +7,33 @@ function App() {
   const [count, setCount] = useState(0);
   const [count2, setCount2] = useState(0);
 
-  const result = Sum(4 * count + count2)
+  const result = useMemo(() => (Sum(4 * count + count2), console.log("asdasd")), [count])
 
   useLayoutEffect(() => {
     console.log("count", count);
   }, [count])
 
   return (
-    <article
-      className="App"
-      value={count}>
+    <article className="App" value={count}>
       <h2>Количество: {count}</h2>
       <h2>+100: {count2}</h2>
+
       <button onClick={() => {
         setCount((prev) => prev - 1)
       }}>
-        Уменьшить
+        Уменьшить (setCount)
       </button>
+
       <button onClick={() => {
         setCount((prev) => prev + 1)
       }}>
-        Увеличть
+        Увеличть (setCount)
       </button>
+
       <button onClick={() => {
         setCount2((prev) => prev + 100)
       }}>
-        +100
+        +100 (setCount2)
       </button>
     </article >
   )
